67510547b6487437dc6fcbf6ae716145b145a5c0
iminlikewithyou <christian@omg.games>
2024-03-25 19:51:20 -0500

    fail on duplicate content

--- .github/scripts/check_diffs.py
+++ .github/scripts/check_diffs.py
@@ -26,6 +26,7 @@ def get_diff_issues(diff_text):
     lines = diff_text.split('\n')
     previous_line_empty = False
     has_content = False
+    content_lines = []
     issues = []
 
     for line_number, line in enumerate(lines, start=1):
@@ -41,6 +42,10 @@ def get_diff_issues(diff_text):
             content = line[1:].strip().upper()  # Retrieve the content, stripping leading and trailing spaces and converting to uppercase
             if content:
                 expected_line = line[0] + " " + content  # Create a properly formatted added/removed line
+                if not expected_line in content_lines:
+                    content_lines.append(expected_line)
+                else:
+                    issues.append(f"{bcolors.FAIL}[error] Invalid line on line {line_number}. Duplicate content detected.{bcolors.ENDC}")
                 previous_line_empty = False
                 has_content = True
                 if expected_line != line:
