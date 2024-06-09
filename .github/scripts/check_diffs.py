f7dc49fb6f9c147528593cdd063c8bbb6f725f27
iminlikewithyou <christian@omg.games>
2024-06-08 21:40:10 -0500

    continue to overcomplicate

--- .github/scripts/check_diffs.py
+++ .github/scripts/check_diffs.py
@@ -1,29 +1,20 @@
 import sys
 import glob
 
-class bcolors:
-  HEADER = '\033[95m'
-  OKBLUE = '\033[94m'
-  OKCYAN = '\033[96m'
-  OKGREEN = '\033[92m'
-  WARNING = '\033[93m'
-  FAIL = '\033[91m'
-  ENDC = '\033[0m'
-  BOLD = '\033[1m'
-  UNDERLINE = '\033[4m'
+from utils.colors import Colors
 
-def get_diff_issues(diff_text):
+def get_diff_issues(diff_content):
   """
-  Retrieves issues in a diff file.
+  Retrieves issues in a diff, formatted for printing to the console.
   
   Parameters:
-  - diff_text: A string representing the contents of a diff file.
+  - diff_content: The content of the diff.
   
   Returns:
-  - A list of strings, each representing an issue found in the diff file.
+  - A list of strings, each representing a printable issue found in the diff file.
   """
 
-  lines = diff_text.split('\n')
+  lines = diff_content.split('\n')
   previous_line_empty = False
   has_content = False
   content_lines = []
@@ -36,7 +27,7 @@ def get_diff_issues(diff_text):
       expected_line = "#" + " " + comment_text  # Create a properly formatted comment line
       previous_line_empty = False
       if expected_line != line:
-        issues.append(f"{bcolors.WARNING}[warning] Invalid line on line {line_number}. Invalid formatting.\nActual:\t[{line}]\nExpected:\t[{expected_line}]{bcolors.ENDC}")
+        issues.append(f"{Colors.WARNING}[warning] Invalid line on line {line_number}. Invalid formatting.\nActual:\t[{line}]\nExpected:\t[{expected_line}]{Colors.ENDC}")
     elif line.startswith(("+", "-")):
       # Added/removed lines
       content = line[1:].strip().upper()  # Retrieve the content, stripping leading and trailing spaces and converting to uppercase
@@ -45,13 +36,13 @@ def get_diff_issues(diff_text):
         if not content in content_lines:
           content_lines.append(content)
         else:
-          issues.append(f"{bcolors.FAIL}[error] Invalid line on line {line_number}. Duplicate content detected: {content}.{bcolors.ENDC}")
+          issues.append(f"{Colors.FAIL}[error] Invalid line on line {line_number}. Duplicate content detected: {content}.{Colors.ENDC}")
         previous_line_empty = False
         has_content = True
         if expected_line != line:
-          issues.append(f"{bcolors.FAIL}[error] Invalid line on line {line_number}. Invalid formatting.\nActual:\t[{line}]\nExpected:\t[{expected_line}]{bcolors.ENDC}")
+          issues.append(f"{Colors.FAIL}[error] Invalid line on line {line_number}. Invalid formatting.\nActual:\t[{line}]\nExpected:\t[{expected_line}]{Colors.ENDC}")
       else:
-        issues.append(f"{bcolors.FAIL}[error] Invalid line on line {line_number}. No content after {line[0]}.{bcolors.ENDC}")
+        issues.append(f"{Colors.FAIL}[error] Invalid line on line {line_number}. No content after {line[0]}.{Colors.ENDC}")
     elif line.strip() == "":
       # Empty lines
       if not previous_line_empty:
@@ -59,18 +50,18 @@ def get_diff_issues(diff_text):
         previous_line_empty = True
       else:
         # Consecutive empty lines
-        issues.append(f"{bcolors.WARNING}[warning] Invalid line on line {line_number}. Consecutive empty lines.{bcolors.ENDC}")
+        issues.append(f"{Colors.WARNING}[warning] Invalid line on line {line_number}. Consecutive empty lines.{Colors.ENDC}")
     else:
       # Invalid character
-      issues.append(f"{bcolors.FAIL}[error] Invalid line on line {line_number}. Must start with +, -, or #.{bcolors.ENDC}")
+      issues.append(f"{Colors.FAIL}[error] Invalid line on line {line_number}. Must start with +, -, or #.{Colors.ENDC}")
 
   if not has_content:
     # No content found in diff
-    issues.append(f"{bcolors.FAIL}[error] No content found in diff.{bcolors.ENDC}")
+    issues.append(f"{Colors.FAIL}[error] No content found in diff.{Colors.ENDC}")
   else:
     # Check for leading and trailing empty lines
     if lines[0] == "":
-      issues.append(f"{bcolors.WARNING}[warning] Leading empty lines detected.{bcolors.ENDC}")
+      issues.append(f"{Colors.WARNING}[warning] Leading empty lines detected.{Colors.ENDC}")
     
   return issues
 
@@ -83,13 +74,13 @@ for file_path in diff_files:
   # Check if this is a diff file
   if not file_path.endswith(".diff"):
     has_issues = True
-    print(f"{bcolors.BOLD}{bcolors.FAIL}[failure]{bcolors.ENDC}{bcolors.FAIL} Non-diff file found in the proposals directory: {file_path}{bcolors.ENDC}")
+    print(f"{Colors.BOLD}{Colors.FAIL}[failure]{Colors.ENDC}{Colors.FAIL} Non-diff file found in the proposals directory: {file_path}{Colors.ENDC}")
     continue
   # Get issues in the diff file
   issues = get_diff_issues(open(file_path, 'r').read())
   if issues:
     has_issues = True
-    print(f"{bcolors.BOLD}{bcolors.FAIL}[failure]{bcolors.ENDC}{bcolors.FAIL} Issues found in {file_path}:{bcolors.ENDC}")
+    print(f"{Colors.BOLD}{Colors.FAIL}[failure]{Colors.ENDC}{Colors.FAIL} Issues found in {file_path}:{Colors.ENDC}")
     for issue in issues:
       print(issue)
 
