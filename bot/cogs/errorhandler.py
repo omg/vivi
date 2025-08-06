7c409e5b194d51b15751c7be0a0568163c20dac6
iminlikewithyou <christian@omg.games>
2025-08-06 14:33:31 -0500

    refactor

--- bot/cogs/errorhandler.py
+++ bot/cogs/errorhandler.py
@@ -1,5 +1,3 @@
-import sys
-import traceback
 import discord
 from discord.ext import commands
 
@@ -22,30 +20,6 @@ class CommandErrorHandler(commands.Cog):
                 return
             
         await interaction.response.send_message(str(error), ephemeral=True)
-
-    # @commands.Cog.listener()
-    # async def on_command_error(self, ctx: commands.Context, error):
-    #     if hasattr(ctx.command, "on_error"):
-    #         # if the command has its own error handler, we dont want to overwrite it
-    #         # nor do we want to handle it twice
-    #         return
-        
-    #     cog = ctx.cog
-    #     if cog:
-    #         if cog._get_overridden_method(cog.cog_command_error) is not None:
-    #             # if the COG has its own error handler, we dont want to overwrite it
-    #             return
-        
-    #     ignored = (commands.CommandNotFound, commands.NoPrivateMessage)
-    #     error = getattr(error, "original", error)
-
-    #     if isinstance(error, ignored):
-    #         return
-        
-    #     elif isinstance(error, ProposalBotError):
-    #         msg = await ctx.send(error.message)
-    #         await msg.delete(delay=15)
-
 
 async def setup(bot):
     await bot.add_cog(CommandErrorHandler(bot))
